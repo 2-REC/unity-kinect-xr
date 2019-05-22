f397fe1b8630a77f16f3bbc4697f11299d8d4d6c
Derek Severin <severinderek@gmail.com>
2019-05-22 23:27:53 +0700

    Safer scripts (checks & pushd+popd)

--- apps/set_links.bat
+++ apps/set_links.bat
@@ -1,40 +1,59 @@
-echo off
+@echo off
 
 rem MAKE A LOOP INSTEAD OF REPEATING FOR EACH PROJECT
 
-cd Main
-cd Assets
 
-md StreamingAssets
-cd StreamingAssets
-mklink /D .node "..\..\..\Libraries\network\Assets\StreamingAssets\.node
-mklink /D .spacebrew "..\..\..\Libraries\network\Assets\StreamingAssets\.spacebrew
-cd..
+IF exist Main (
+    pushd .
 
-md SpaceBrew
-cd SpaceBrew
-mklink /D Editor "..\..\..\Libraries\network\Assets\SpaceBrew\Editor
-mklink /D NodeJs "..\..\..\Libraries\network\Assets\SpaceBrew\NodeJs
-mklink /D Plugins "..\..\..\Libraries\network\Assets\SpaceBrew\Plugins
-mklink /D Prefabs "..\..\..\Libraries\network\Assets\SpaceBrew\Prefabs
-mklink /D Scripts "..\..\..\Libraries\network\Assets\SpaceBrew\Scripts
-cd ../../..
+    cd Main
+    cd Assets
+    
+    md StreamingAssets
+    cd StreamingAssets
+    mklink /D .node "..\..\..\Libraries\network\Assets\StreamingAssets\.node
+    mklink /D .spacebrew "..\..\..\Libraries\network\Assets\StreamingAssets\.spacebrew
+    cd..
+    
+    md SpaceBrew
+    cd SpaceBrew
+    mklink /D Editor "..\..\..\Libraries\network\Assets\SpaceBrew\Editor
+    mklink /D NodeJs "..\..\..\Libraries\network\Assets\SpaceBrew\NodeJs
+    mklink /D Plugins "..\..\..\Libraries\network\Assets\SpaceBrew\Plugins
+    mklink /D Prefabs "..\..\..\Libraries\network\Assets\SpaceBrew\Prefabs
+    mklink /D Scripts "..\..\..\Libraries\network\Assets\SpaceBrew\Scripts
+    cd ..
+
+    popd
+
+) ELSE (
+    echo 'Main' doesn't exist!
+)
 
 
-cd Client
-cd Assets
+IF exist Client (
+    pushd .
+    
+    cd Client
+    cd Assets
+    
+    md StreamingAssets
+    cd StreamingAssets
+    mklink /D .node "..\..\..\Libraries\network\Assets\StreamingAssets\.node
+    mklink /D .spacebrew "..\..\..\Libraries\network\Assets\StreamingAssets\.spacebrew
+    cd..
+    
+    md SpaceBrew
+    cd SpaceBrew
+    mklink /D Editor "..\..\..\Libraries\network\Assets\SpaceBrew\Editor
+    mklink /D NodeJs "..\..\..\Libraries\network\Assets\SpaceBrew\NodeJs
+    mklink /D Plugins "..\..\..\Libraries\network\Assets\SpaceBrew\Plugins
+    mklink /D Prefabs "..\..\..\Libraries\network\Assets\SpaceBrew\Prefabs
+    mklink /D Scripts "..\..\..\Libraries\network\Assets\SpaceBrew\Scripts
+    cd ..
+    
+    popd
 
-md StreamingAssets
-cd StreamingAssets
-mklink /D .node "..\..\..\Libraries\network\Assets\StreamingAssets\.node
-mklink /D .spacebrew "..\..\..\Libraries\network\Assets\StreamingAssets\.spacebrew
-cd..
-
-md SpaceBrew
-cd SpaceBrew
-mklink /D Editor "..\..\..\Libraries\network\Assets\SpaceBrew\Editor
-mklink /D NodeJs "..\..\..\Libraries\network\Assets\SpaceBrew\NodeJs
-mklink /D Plugins "..\..\..\Libraries\network\Assets\SpaceBrew\Plugins
-mklink /D Prefabs "..\..\..\Libraries\network\Assets\SpaceBrew\Prefabs
-mklink /D Scripts "..\..\..\Libraries\network\Assets\SpaceBrew\Scripts
-cd ../../..
+) ELSE (
+    echo 'Client' doesn't exist!
+)
